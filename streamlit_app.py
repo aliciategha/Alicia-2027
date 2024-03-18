4a2f55c79c33be8ae89bb678c7edfc5dd526a9f2
aliciategha <162982987+aliciategha@users.noreply.github.com>
2024-03-18 12:36:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,10 @@
 import streamlit as st 
 import pandas as pd 
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTCnNUKxiK_6WUL2l3_S2MhTWB744cSf9tXzOg1edajF2pjDyeomM6Kz_W9pR6YCx1MgLjVNt_znLqV/pub?output=csv') 
-st.dataframe(voc)
+l=voc.shape[0]
+i=np.random.choice(range(l))
+word_fr=voc["Définition"].values[i]
+word_pin=["Pinyin"].values[i]
+word_chi=["Hanzi"].values[i]
+st.write(word_fr+" "+word_pin+" "+word_chi)
+st.button("refresh")
