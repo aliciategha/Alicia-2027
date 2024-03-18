3a0ce88c2114f67d90742ef6f0897a2674894de5
aliciategha <162982987+aliciategha@users.noreply.github.com>
2024-03-18 11:34:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
+
 import streamlit as st 
-st.write("Hello") 
-name=st.text_input("Your name") 
-st.write("Hello" + name )
-genre = st.radio(
-    "chien/chat",
-    [":chien", "chat:"]
+import pandas as pd 
+voc=pd.need_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTCnNUKxiK_6WUL2l3_S2MhTWB744cSf9tXzOg1edajF2pjDyeomM6Kz_W9pR6YCx1MgLjVNt_znLqV/pub?output=csv") 
+st.dataframe(voc)
